68dae501cff3dc2796d2256324c4fda4431752cb
Martin <martinmozzt@pm.me>
2021-10-14 16:01:53 -0300

    Config for deploy

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js')
 const { conn } = require('./src/db.js')
 
 conn.sync().then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('SERVER LISTENING AT PORT 3001')
   })
 })
--- api/src/app.js
+++ api/src/app.js
@@ -13,6 +13,15 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
 server.use(bodyParser.json({ limit: '50mb' }))
 server.use(cookieParser())
 server.use(cors())
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
 server.use('/', require('./routes'))
 
